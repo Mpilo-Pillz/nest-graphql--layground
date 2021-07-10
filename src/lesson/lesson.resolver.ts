ca6b8d26fe8d7e99a766c1099a4fd15582390c2c
Fikasentani <mpilo.dlamini@yahoo.com>
2021-07-10 16:30:56 +0200

    initial commit intial working state

---
+++ src/lesson/lesson.resolver.ts
@@ -0,0 +1,16 @@
+import { Resolver, Query } from '@nestjs/graphql';
+import { LessonType } from './lesson.type';
+
+//equivalent to controller
+@Resolver((of) => LessonType)
+export class LessonResolver {
+  @Query((returns) => LessonType)
+  lesson() {
+    return {
+      id: '57845t4r5t45',
+      name: 'TypeScript Class',
+      startDate: new Date().toISOString(),
+      endDate: new Date().toISOString(),
+    };
+  }
+}
