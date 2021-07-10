9470896de622210adcaab98ef59ca8b07ee47ea4
Fikasentani <mpilo.dlamini@yahoo.com>
2021-07-10 17:16:00 +0200

    implementing create lesson

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -1,9 +1,11 @@
-import { Resolver, Query, Mutation } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import { LessonService } from './lesson.service';
 import { LessonType } from './lesson.type';
 
 //equivalent to controller
 @Resolver((of) => LessonType)
 export class LessonResolver {
+  constructor(private lessonService: LessonService) {}
   @Query((returns) => LessonType)
   lesson() {
     return {
@@ -15,5 +17,11 @@ export class LessonResolver {
   }
 
   @Mutation((returns) => LessonType)
-  createLesson() {}
+  createLesson(
+    @Args('name') name: string,
+    @Args('startDate') startDate: string,
+    @Args('endDate') endDate: string,
+  ) {
+    return this.lessonService.createLesson(name, startDate, endDate);
+  }
 }
